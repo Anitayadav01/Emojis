e4eb6c0bd6665929e5d9ac2701e13cfe8b17d8ef
Anita Yadav <anita.afria@gmail.com>
2018-10-06 13:51:55 -0500

    added emojis

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-const emoji = require('emoji');
 
+
+const emoji = require('node-emoji');
+let emojis = ["cherry_blossom","rose","hibiscus","sunflower","cherries","raised_hand","ocean","lollipop","leaves","custard"];
+function loopEmoji(arr) {
+arr.forEach((element)=>{
+    console.log(emoji.get(element));
+})
+}
+  loopEmoji(emojis);
